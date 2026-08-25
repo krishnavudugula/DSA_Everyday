cc5a85a579430fdd73a04b3efcfd5a2153208c06
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-08-25 20:59:19 +0530

    Check if contains duplicates in an array - 2nd Approach (Set)

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -1,12 +1,20 @@
 import java.util.*;
 public class ContainsDuplicate {
     public static boolean containsDuplicate(int[] nums){
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        for(int i=0;i<nums.length-1; i++){
-            if(nums[i] == nums[i+1]){
+        // for(int i=0;i<nums.length-1; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums){
+            if(set.contains(num)){
                 return true;
             }
+            set.add(num);
         }
         return false;
     }
