e7b11772c3f79d63339cc1a63ee9185e85396611
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-08-25 20:12:14 +0530

    Check if contains duplicates in an array

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums){
+        Arrays.sort(nums);
+
+        for(int i=0;i<nums.length-1; i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int nums[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+        boolean result = containsDuplicate(nums);
+        System.out.println(result);
+    }
+}
