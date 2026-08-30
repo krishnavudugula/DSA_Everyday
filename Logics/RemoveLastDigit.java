60145167b31cfc23ca9afd216f4edde951390605
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-08-30 11:44:17 +0530

    Remove Last Digit

---
+++ Logics/RemoveLastDigit.java
@@ -0,0 +1,7 @@
+public class RemoveLastDigit {
+    public static void main(String[] args) {
+        int num = 9876;
+        int removeLast = num / 10;
+        System.out.println(removeLast);
+    }
+}
