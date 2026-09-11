0fe6b80c9984029491d730f6b84b29dcbe81b33b
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-11 09:10:10 +0530

    1.Two Sum - HashMap Approach

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -17,6 +17,24 @@ public class TwoSum {
         return new int[]{};
     }
 
+//     class Solution {    ---> Using HashMap for better time complexity
+//     public int[] twoSum(int[] nums, int target) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+        
+//         for(int i = 0; i < nums.length; i++) {
+//             int complement = target - nums[i];
+            
+//             if(map.containsKey(complement)) {
+//                 return new int[]{map.get(complement), i};
+//             }
+            
+//             map.put(nums[i], i);
+//         }
+        
+//         return new int[]{};
+//     }
+// }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
