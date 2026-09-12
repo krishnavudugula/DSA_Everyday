eaf7956123cdf628e6c3dab5e9fa63f561685f82
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-12 18:40:41 +0530

    485. Max Consecutive Ones - Userdefined

---
+++ Arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,33 @@
+//485
+
+import java.util.Scanner;
+
+public class MaxConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums){
+        int count = 0;
+        int max = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+                max = Math.max(max, count); 
+            }
+            else {
+                count = 0;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.printf("Enter %d elements (0 or 1) : ", n );
+
+        for(int i=0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+        int result = findMaxConsecutiveOnes(nums);
+        System.out.println(result);
+    }
+}
