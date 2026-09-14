f5389c3f4c9a320332533f2c7bbe7cdbc65c67c2
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-14 23:01:05 +0530

    977. Squares of a sorted Array - Userdefined

---
+++ Arrays/SortedSquares.java
@@ -0,0 +1,41 @@
+import java.util.*;
+//977. Squares of a sorted Array (could be negative numbers too)
+public class SortedSquares {
+    public static int[] sortedSquares(int[] nums){
+        int n = nums.length;
+
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[i] = leftSquare;
+                left++;
+            } else {
+                result[i] = rightSquare;
+                right--;
+            }
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for(int i=0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        int[] result = sortedSquares(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
