b7e340ef7a2fcd068430f22cffb4bc09bb4ed7a5
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-19 10:05:14 +0530

    135. Candies - Userdefined

--- Arrays/Chocolate.java
+++ Arrays/Chocolate.java
@@ -1,31 +1,36 @@
 import java.util.*;
+//135. Candies
 public class Chocolate {
     public static int candy(int[] ratings){
         int n = ratings.length;
         int[] candies = new int[n];
-        for (int i = 0; i < n; i++) {
-            candies[i] = 1;
-        }
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+        Arrays.fill(candies, 1);
+
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1] + 1;
             }
         }
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = Math.max(candies[i], candies[i+1] + 1);
             }
         }
         int totalCandies = 0;
-        for (int candy : candies) {
+
+        for(int candy : candies){
             totalCandies += candy;
         }
         return totalCandies;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ratings[] = {1,2,1,3,1,2,1};
+        int m = sc.nextInt();
+        int[] ratings = new int[m];
 
-        System.out.println((candy(ratings)));
+        for(int i=0; i<m; i++){
+            ratings[i] = sc.nextInt();
+        }
+        System.out.println(candy(ratings));
     }
 }
